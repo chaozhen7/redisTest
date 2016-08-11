698654047f07d401887ce28851b79c8664ae1899
Chaos_sina <Chaos@sina.com>
2016-08-11 11:27:06 +0800

    pedis 对redis的操作

---
+++ src/util/Test.java
@@ -0,0 +1,14 @@
+package util;
+/**
+ * @author sina Email:chaozhen7@163.com
+ * @date 2016年8月11日 上午11:15:01
+ * @version 1.0
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new RedisClient().show();
+	}
+
+}
